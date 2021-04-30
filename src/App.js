4e4d71fac31401e11a23585cdccba3b4a86a5934
binoy <binoy.18@cse.mrt.ac.lk>
2021-04-30 09:40:08 +0530

    news card added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { useEffect, useState } from 'react';
+import NewsCards from './components/newsCards/newsCards';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>alan ai</h1>
+const alanKey='06a16214f943a579508c01beceda83ad2e956eca572e1d8b807a3e2338fdd0dc/stage'
+
+
+const App = () => {
+
+  const [newsArticles, setNewsArticles]= useState([]);
+
+  useEffect(()=>{
+    alanBtn({
+      key:alanKey,
+      onCommand:({command,articles})=>{
+        if(command === 'newHeadlines'){
+          setNewsArticles(articles);
+        }
+      },
+    });
+
+  },[])
+
+  return (  
+    <div >
+      <h1>ALAN AI NEWS APP</h1>
+      <NewsCards articles={newsArticles}/>
     </div>
   );
 }
-
+ 
 export default App;
