84c14e6861e55ff84e12764a608c4628508643fb
binoy <binoy.18@cse.mrt.ac.lk>
2021-04-30 16:47:29 +0530

    home screen created

--- src/components/newsCards/newsCards.js
+++ src/components/newsCards/newsCards.js
@@ -2,11 +2,47 @@ import NewsCard from "./newsCard/newsCard";
 import {Grid,Grow,Typography} from '@material-ui/core';
 import useStyles from './styles.js';
 
+const infoCards = [
+    { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
+    { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
+    { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
+    { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me BBC news highlights' },
+  ];
 
 const NewsCards = ({articles}) => {
     const classes = useStyles();
-    return (  
-        <div>
+    //if no artiles
+    if(!articles.length){
+        return(
+            <Grow in>
+                <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                    {infoCards.map((info)=>(
+                        <Grid className={classes.infoCard} item xs={12} sm={6} md={4} lg={3} style={{display:"flex"}}>
+                            <div className={classes.card} style={{background: info.color}}>
+                                <Typography  variant="h5"> {info.title}</Typography>
+
+                                {info.info ? 
+                                (<Typography variant="h6">
+                                    <strong>{info.title.split(' ')[2]}</strong> 
+                                    <br/>
+                                    {info.info}
+                                </Typography>)
+                                : null
+                                }
+
+                                <Typography variant="h7" > Try Saying: {info.text}</Typography>
+                            </div>
+                        </Grid>
+                    ))}
+                    
+                </Grid>
+                
+            </Grow>
+
+        );
+    }
+    else{
+        return (  
             <Grow in>
                 <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {articles.map((article,i)=>(
@@ -20,9 +56,10 @@ const NewsCards = ({articles}) => {
                 
             </Grow>
             
-            
-        </div>
     );
+
+    }
+    
 }
  
 export default NewsCards;
